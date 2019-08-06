2c674242898b9b10f6c70cbf794e0b831e442aef
Gabriel Arguello Costta <gio666nb@gmail.com>
2019-08-06 16:48:00 -0500

    Agregando Componente de gestion de usuarios

---
+++ src/app/gestion-usuarios/gestion-usuarios.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {SelectItem} from 'primeng/api';
+
+@Component({
+  selector: 'app-gestion-usuarios',
+  templateUrl: './gestion-usuarios.component.html',
+  styleUrls: ['./gestion-usuarios.component.css']
+})
+export class GestionUsuariosComponent implements OnInit {
+
+  cities1 = []
+
+  constructor() { 
+    this.cities1 = [
+      {label:'New York', value:{id:1, name: 'New York', code: 'NY'}},
+      {label:'Rome', value:{id:2, name: 'Rome', code: 'RM'}},
+      {label:'London', value:{id:3, name: 'London', code: 'LDN'}},
+      {label:'Istanbul', value:{id:4, name: 'Istanbul', code: 'IST'}},
+      {label:'Paris', value:{id:5, name: 'Paris', code: 'PRS'}}
+  ];
+  }
+
+  ngOnInit() {
+  }
+
+}
